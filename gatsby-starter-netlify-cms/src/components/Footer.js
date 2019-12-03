ab2f95f3df33eaf0ae336c100f35b0ee9210362d
Victor <victorasauceda@gmail.com>
2019-12-02 17:03:07 -0800

    changed products to services

--- gatsby-starter-netlify-cms/src/components/Footer.js
+++ gatsby-starter-netlify-cms/src/components/Footer.js
@@ -36,7 +36,7 @@ const Footer = class extends React.Component {
                     </li>
                     <li>
                       <Link className="navbar-item" to="/products">
-                        Products
+                        Services
                       </Link>
                     </li>
 
