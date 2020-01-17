d6783d40385e14ef4ccb03f4b32100981f7cf4f0
Victor <victorasauceda@gmail.com>
2020-01-17 15:31:04 -0800

    media queries for css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import MediaQuery from "react-responsive";
+import "./app.css";
 // import github from "../img/github-icon.svg";
 // import logo from "../img/logo.svg";
 
@@ -40,60 +41,90 @@ const Navbar = class extends React.Component {
         className="navbar is-transparent"
         role="navigation"
         aria-label="main-navigation"
+        style={{ height: "4.5rem" }}
       >
-        <MediaQuery minDeviceWidth={1224}>
-          <div
-            style={{
-              display: "flex !important",
-              flexDirection: "row",
-              alignItems: "flex-start"
-            }}
-            className="container"
-          >
-            <div className="navbar-brand">
-              <Link to="/" className="navbar-item" title="Logo">
-                <img
-                  src="../../img/pcmlogoonly.png"
-                  alt="PCM logo"
+        <div
+          style={{
+            display: "flex !important",
+            flexDirection: "row",
+            alignItems: "flex-start"
+          }}
+          className="container"
+        >
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item" title="Logo">
+              <img
+                id="logo"
+                src="../../img/pcmlogoonly.png"
+                alt="PCM logo"
+                style={{
+                  width: "136px",
+                  height: "auto",
+                  padding: "0px !important"
+                }}
+              />
+            </Link>
+            <div id="buttonHelper">
+              <Link to="/contact">
+                <Button
+                  className="btn btn-secondary"
                   style={{
-                    width: "136px",
-                    height: "auto",
-                    padding: "0px !important"
+                    color: "white",
+                    backgroundColor: "#0072bc",
+                    boxShadow: "inset 0 0 0 2px #0072bc",
+                    marginTop: "4px",
+                    marginRight: "1.25rem"
                   }}
-                />
+                >
+                  Request A FREE Quote
+                </Button>
               </Link>
-              {/* Hamburger menu */}
-              <div
-                className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-                data-target="navMenu"
-                onClick={() => this.toggleHamburger()}
-              >
-                <span />
-                <span />
-                <span />
-              </div>
+              <a href="tel:+12067417626">
+                <Button
+                  className="btn btn-secondary float-right"
+                  style={{
+                    color: "white",
+                    backgroundColor: "#0072bc",
+                    boxShadow: "inset 0 0 0 2px #0072bc",
+                    marginTop: "4px",
+                    marginRight: "1.25rem"
+                  }}
+                >
+                  Call Now
+                </Button>
+              </a>
             </div>
+            {/* Hamburger menu */}
             <div
-              id="navMenu"
-              className={`navbar-menu ${this.state.navBarActiveClass}`}
+              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+              data-target="navMenu"
+              onClick={() => this.toggleHamburger()}
             >
-              <div
-                className="navbar-start has-text-centered"
-                style={{ width: "100%", justifyContent: "space-between" }}
-              >
-                <div style={{ display: "flex" }}>
-                  <Link className="navbar-item" to="/about">
-                    About
-                  </Link>
-                  <Link className="navbar-item" to="/services">
-                    Services
-                  </Link>
+              <span />
+              <span />
+              <span />
+            </div>
+          </div>
+          <div
+            id="navMenu"
+            className={`navbar-menu ${this.state.navBarActiveClass}`}
+          >
+            <div
+              className="navbar-start"
+              style={{ width: "100%", justifyContent: "space-between" }}
+            >
+              <div id="linkDiv" style={{ display: "flex" }}>
+                <Link className="navbar-item" to="/about">
+                  About
+                </Link>
+                <Link className="navbar-item" to="/services">
+                  Services
+                </Link>
 
-                  <Link className="navbar-item" to="/contact">
-                    Contact
-                  </Link>
-                </div>
-                <div>
+                <Link className="navbar-item" to="/contact">
+                  Contact
+                </Link>
+                <div id="buttonHelperBig">
                   <Link to="/contact">
                     <Button
                       className="btn btn-secondary"
@@ -126,93 +157,7 @@ const Navbar = class extends React.Component {
               </div>
             </div>
           </div>
-        </MediaQuery>
-        <MediaQuery maxDeviceWidth={1224}>
-          <div
-            style={{
-              display: "flex !important",
-              flexDirection: "column",
-              alignItems: "flex-start"
-            }}
-            className="container"
-          >
-            <div className="navbar-brand" style={{ display: "flex" }}>
-              <Link to="/" className="navbar-item" title="Logo">
-                <img
-                  src="../../img/pcmlogoonly.png"
-                  alt="PCM logo"
-                  style={{
-                    width: "136px",
-                    height: "auto",
-                    padding: "0px !important"
-                  }}
-                />
-              </Link>
-              {/* Hamburger menu */}
-              <div
-                className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-                data-target="navMenu"
-                onClick={() => this.toggleHamburger()}
-              >
-                <span />
-                <span />
-                <span />
-              </div>
-            </div>
-            <div
-              id="navMenu"
-              className={`navbar-menu ${this.state.navBarActiveClass}`}
-            >
-              <div
-                className="navbar-start has-text-centered"
-                style={{ width: "100%", justifyContent: "space-between" }}
-              >
-                <div style={{ display: "flex" }}>
-                  <Link className="navbar-item" to="/about">
-                    About
-                  </Link>
-                  <Link className="navbar-item" to="/services">
-                    Services
-                  </Link>
-
-                  <Link className="navbar-item" to="/contact">
-                    Contact
-                  </Link>
-                </div>
-                <div>
-                  <Link to="/contact">
-                    <Button
-                      className="btn btn-secondary"
-                      style={{
-                        color: "white",
-                        backgroundColor: "#0072bc",
-                        boxShadow: "inset 0 0 0 2px #0072bc",
-                        marginTop: "4px",
-                        marginRight: "1.25rem"
-                      }}
-                    >
-                      Request A FREE Quote
-                    </Button>
-                  </Link>
-                  <a href="tel:+12067417626">
-                    <Button
-                      className="btn btn-secondary float-right"
-                      style={{
-                        color: "white",
-                        backgroundColor: "#0072bc",
-                        boxShadow: "inset 0 0 0 2px #0072bc",
-                        marginTop: "4px",
-                        marginRight: "1.25rem"
-                      }}
-                    >
-                      Call Now
-                    </Button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </MediaQuery>
+        </div>
       </nav>
     );
   }
